9b8648598584d35863fb3a9c83303579b7532201
hj3j <minhchaup780q@gmail.com>
2026-08-24 18:16:14 +0700

    TICKET-13 fix: sửa lại những message lỗi được throw trong auth-service

--- Backend/auth-service/src/main/java/edu/vn/iuh/fit/authservice/controllers/AuthenController.java
+++ Backend/auth-service/src/main/java/edu/vn/iuh/fit/authservice/controllers/AuthenController.java
@@ -4,11 +4,16 @@ import edu.vn.iuh.fit.authservice.dto.request.LoginRequest;
 import edu.vn.iuh.fit.authservice.dto.response.LoginResponse;
 import edu.vn.iuh.fit.authservice.services.AuthenticationService;
 import lombok.RequiredArgsConstructor;
+import org.apache.http.protocol.HTTP;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -19,8 +24,8 @@ public class AuthenController {
 
     @PostMapping("/login")
     public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest request){
-         LoginResponse loginResponse = authenticationService.login(request);
-         return ResponseEntity.ok().body(loginResponse);
+            LoginResponse loginResponse = authenticationService.login(request);
+            return ResponseEntity.ok().body(loginResponse);
     }
 
 }
--- Backend/user-service/src/main/java/edu/vn/iuh/fit/userservice/services/impl/UserServiceImpl.java
+++ Backend/user-service/src/main/java/edu/vn/iuh/fit/userservice/services/impl/UserServiceImpl.java
@@ -6,6 +6,8 @@ import edu.vn.iuh.fit.userservice.dto.response.UserCreateResponse;
 import edu.vn.iuh.fit.userservice.entities.User;
 import edu.vn.iuh.fit.userservice.repositories.UserRepository;
 import edu.vn.iuh.fit.userservice.services.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -20,7 +22,7 @@ public class UserServiceImpl implements UserService {
     @Override
     public UserCreateResponse createUser(UserCreateRequest request) {
         if(userRepository.existsByEmail(request.getEmail())){
-            throw new RuntimeException("Email đã tồn tại! Vui lòng nhập email khác");
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Email đã tồn tại! Vui lòng nhập email khác");
         }
 
         PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
@@ -45,7 +47,7 @@ public class UserServiceImpl implements UserService {
         if(userRepository.existsByEmail(email)){
             user = userRepository.findUsersByEmail(email);
         }else {
-            throw new RuntimeException("Email chưa được đăng kí!");
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Email chưa được đăng ký!");
         }
 
         return UserAuthResponse.builder()
